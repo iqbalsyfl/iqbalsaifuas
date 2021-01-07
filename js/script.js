0dbdad65ea0576a27dc8cedd746f36d2d8d13e92
iqbalsyfl <iqbalsaif4n1@gmail.com>
2021-01-07 23:32:20 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function imgSlider(anything) {
+    document.querySelector('.stall').src = anything;
+}
+
+function changeCirlceColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+function changeCirlceColorMenu(color) {
+    const circle = document.querySelector('.circle-m');
+    circle.style.background = color;
+}
